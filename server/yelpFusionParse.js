25b235bae30ad340100ee09056cfe158fee22087
Tri Dao <tvdao@ucsd.edu>
2023-03-22 13:50:15 -0700

    Add yelp fusion parser to db

--- server/yelpFusionParse.js
+++ server/yelpFusionParse.js
@@ -1,19 +1,50 @@
 'use strict';
 
-// const yelp = require('yelp-fusion');
-// const client = yelp.client('rB-uSi1Q117G_onjgYyPdz1vbR5_-O5_TpE2vtRT08iV3XbYPf4j5TUYIyVkf6TXUuDkjcZEuwrg29BOTObmOwa-HAmDGOQ4x3JgyGXj-WHXsdGalLuUg0RrXBYZZHYx');
+require("dotenv").config();
+const express = require("express")
+const db = require("./db/index.js");
 
-// for (let i = 0; i < caliCities.length; i++) {
-//   client.search({
-//     location: caliCities[i],
-//   }).then(response => {
-//     console.log(response.jsonBody.total);
-//     // for (let i = 0; i < response.jsonBody.total; i++) {
-//     //   // console.log(response.jsonBody.businesses[i]);
-//     //   console.log(i);
-//     // }
-//     // console.log(response.jsonBody.total);
-//   }).catch(e => {
-//     console.log(e);
-//   });
-// }
+const yelp = require('yelp-fusion');
+const client = yelp.client('abaUkx_lHwx2Q32oOhjRVfEb05RCUdlHQsrJxGMo_VoZ8eQLZV5p6tH3rMhxhNqgnSOYZvoBIsMMz3HKE9sObjcXisiiROEFrq30eoOwZOkA27d2Qns0cHS5TZQaZHYx');
+
+// console.log(await getCities());
+
+const getBusinessParams = async () => {
+    db
+    .query('SELECT * from cities')
+    .then(results => {
+        let count = 1000
+        for (let i = count; i < 80+count; i+=5) {
+            setTimeout(() => {
+                for (let j = 0; j < 5; j++) {
+                    client.search({
+                        location: results.rows[i + j].name,
+                    }).then(response => insertRestaurant(response))
+                    .catch(e => {
+                        console.log(e);
+                    });
+                }
+            }, 300 * (i-count));
+        }
+    })
+    .catch(e => console.error(e.stack))
+};
+
+const insertRestaurant = async (response) => {
+    for (const business of response.jsonBody.businesses) {
+        const params = [
+            business.id,
+            business.name,
+            business.location.city,
+            business.location.address1,
+            business.location.zip_code,
+            business.rating,
+            business.price,
+            business.image_url,
+            business.display_phone,
+        ]
+        await db.query(
+            "INSERT INTO restaurants(id, name, city, address, zip, rating, price, image_url, phone_num) values ($1, $2, $3, $4, $5, $6, $7, $8, $9)", params);
+    }
+};
+getBusinessParams();
