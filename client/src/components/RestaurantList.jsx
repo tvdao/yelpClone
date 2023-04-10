71ed7a365b97805e864f76a94b5b19a456ae3609
Tri Dao <tri.dao483@gmail.com>
2023-04-09 21:17:33 -0700

    Add search function

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useContext} from "react";
+import React, { useEffect, useContext } from 'react';
 
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
@@ -8,49 +8,52 @@ import RestaurantFinder from '../apis/RestaurantFinder';
 import { RestaurantsContext } from '../context/RestaurantsContext';
 
 const styles = {
-    cardImage: {
-        width: '20vw',
-        height: '20vh'
-    },
-    card: {
-    }
-}
+  cardImage: {
+    width: '20vw',
+    height: '20vh',
+  },
+  card: {},
+};
 
 const RestaurantList = () => {
-    const { restaurant } = useContext(RestaurantsContext)
-    const [restaurantList, setRestaurantList] = restaurant;
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await RestaurantFinder.get("/");
-                if (response.status === 200) {
-                    setRestaurantList(response.data.data.restaurant)
-                }
-            } catch(err) {
-                console.log(err);
-            }
+  const { restaurant } = useContext(RestaurantsContext);
+  const [restaurantList, setRestaurantList] = restaurant;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get('/');
+        if (response.status === 200) {
+          setRestaurantList(response.data.data.restaurant);
         }
-        fetchData();
-    }, []);
-    
-    return (
-        <Container>
-            {restaurantList.map((item) => (
-                <Card style={styles.card} className="border-0 mt-3" key={item.id}>
-                    <Row>
-                        <Col md="auto">
-                            <Card.Img src={item.image_url} style={styles.cardImage}/>
-                        </Col>
-                        <Col xs={8}>
-                            <Card.Body>
-                                <Card.Text>{item.name}</Card.Text>
-                            </Card.Body>
-                        </Col>
-                    </Row>
-                </Card> 
-            ))}
-        </Container>         
-    )
-}
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
 
-export default RestaurantList
+  return (
+    <Container>
+      {restaurantList !== undefined &&
+        restaurantList.map((item, index) => (
+          <Card style={styles.card} className="border-0 mt-3" key={index}>
+            <Row>
+              <Col md="auto">
+                <Card.Img src={item.image_url} style={styles.cardImage} />
+              </Col>
+              <Col xs={8}>
+                <Card.Body>
+                  <Card.Text>{item.name}</Card.Text>
+                  <Card.Text>{item.address + ' ' + item.zip}</Card.Text>
+                  <Card.Text>{item.price}</Card.Text>
+                  <Card.Text>{'Rating: ' + item.rating + '/' + '5'}</Card.Text>
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
+        ))}
+    </Container>
+  );
+};
+
+export default RestaurantList;
--- client/src/context/RestaurantsContext.js
+++ client/src/context/RestaurantsContext.js
@@ -1,15 +1,20 @@
-import React, {useState, createContext} from 'react';
+import React, { useState, createContext } from 'react';
 
 export const RestaurantsContext = createContext();
 
-export const RestaurantsContextProvider = props => {
-    const [restaurants, setRestaurants] = useState([])
-    const [city, setCity] = useState("")
-    return (
-        <RestaurantsContext.Provider 
-            value={{restaurant: [restaurants, setRestaurants], 
-                    city: [city, setCity]}}>
-            {props.children}
-        </RestaurantsContext.Provider>
-    )
-}
+export const RestaurantsContextProvider = (props) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [city, setCity] = useState('');
+  const [search, setSearch] = useState('');
+  return (
+    <RestaurantsContext.Provider
+      value={{
+        restaurant: [restaurants, setRestaurants],
+        city: [city, setCity],
+        search: [search, setSearch],
+      }}
+    >
+      {props.children}
+    </RestaurantsContext.Provider>
+  );
+};
